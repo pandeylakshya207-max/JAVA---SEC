f2fb2f260a2398ce98e1ad84ae335d3153a627a1
Lakshya Pandey <pandeylakshya207@gmail.com>
2026-08-31 17:48:18 +0530

    Implement palindrome checker in StringPalindrome.java

---
+++ StringPalindrome.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class StringPalindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String reverse = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse = reverse + str.charAt(i);
+        }
+
+        if (str.equalsIgnoreCase(reverse)) {
+            System.out.println("String is palindrome");
+        } else {
+            System.out.println("String is not palindrome");
+        }
+
+        sc.close();
+    }
+}
